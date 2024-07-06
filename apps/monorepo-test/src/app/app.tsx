8dcde365022f44dcf5950c639b06c4879cbc45f9
Ryan Casey <kristin@kristins-laptop.home>
2024-07-06 19:46:20 -0300

    add ui library

--- apps/monorepo-test/src/app/app.tsx
+++ apps/monorepo-test/src/app/app.tsx
@@ -1,12 +1,12 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.scss';
-
 import NxWelcome from './nx-welcome';
+
+import { Ui } from '@monorepo-test/ui';
 
 export function App() {
   return (
     <div>
       <NxWelcome title="monorepo-test" />
+      <Ui />
     </div>
   );
 }
